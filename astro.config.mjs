b5b6456fc1b5ad905ffec939c125b1395c6e4ace
bjsracing <emeraldglobaltrading@gmail.com>
2025-08-05 21:13:08 +0000

    Fix build failed vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,25 @@
-// astro.config.mjs (Versi Final)
+// astro.config.mjs (Versi Final yang Benar)
 
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless"; // Menggunakan /serverless untuk Vercel
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server", // <-- TAMBAHKAN BARIS INI
   integrations: [
     tailwind(),
     react(),
   ],
+
   vite: {
     server: {
       allowedHosts: [
         '.replit.dev'
       ]
     }
-  }
+  },
+
+  adapter: vercel()
 });
